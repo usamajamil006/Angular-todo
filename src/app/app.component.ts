e981a699cea245970deaa3c5bb46c0c23e290c8e
usama-jamil <usamajamil006@gmail.com>
2019-10-12 16:40:52 +0500

    Making component for todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'todo-app';
+  title: string = "Todo App Usama jamil";
+
+  constructor() {
+    console.log(1234);
+    this.title = "Pakistan";
+
+    this.changeName("ansa");
+  }
+
+  changeName(title: string): void {
+    this.title = title;
+  }
 }
